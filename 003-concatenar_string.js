d8d6eeb2b3920e9185bf29712481b08669ce36fb
kbnotek <bruno.q@aluno.senai.br>
2024-05-07 08:57:37 -0300

    Tipo de string

--- 003-concatenar_string.js
+++
@@ -1,6 +0,0 @@
-var dono = "Bruno"
-var ddd = 61
-var telefone = "9 9999-9999"
-
-let texto = `O cliente ${dono} tem o seguinte numero de celular ${ddd} ${telefone}`
-console.log(texto)
---
+++ 003-tipo_string.js
@@ -0,0 +1,15 @@
+var dono = "Bruno"
+var ddd = "61"
+var telefone = "9 9999-9999"
+
+console.log("O Cliente "+dono+" tem o seguinte numero de celular ("+ddd+") "+telefone)
+
+let texto = `O cliente ${dono} tem o seguinte numero de celular  (${ddd}) ${telefone}`
+console.log(texto)
+
+var idade = 20
+console.log(idade +1)
+
+/*console.log(idade.toString() +1)
+console.log("idade e "+idade +1)
+console.log("idade e ",idade +1)*/
