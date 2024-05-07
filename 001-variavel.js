271f2dfe6f9ff767dfe8363879b5be49d4e1b33a
kbnotek <bruno.q@aluno.senai.br>
2024-05-07 10:28:37 -0300

    Variavel

--- 001-variavel.js
+++ 001-variavel.js
@@ -3,16 +3,14 @@
 //LET variavel de escopo
 
 
-if(true)
-{
+if (true) {
     let nome = "Ribeiro"
 }
 console.log(nome)
 
 var nome = 'Constante'
-if(true)
-{
-    const nome ="Jesus"
+if (true) {
+    const nome = "Jesus"
 }
-let nome = "Bruno" 
+let nome = "Bruno"
 console.log(nome)
